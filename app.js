778f30f8b307a22b9951fd36d55db8aab111a6ec
Arnab Sinha <arisin@gmail.com>
2015-09-03 23:13:02 -0700

    adding ng-submit

--- app.js
+++ app.js
@@ -9,13 +9,17 @@ myWeatherApp.service('cityService',function(){
 });
 
 //Controllers
-myWeatherApp.controller('homeController',['$scope', 'cityService', function($scope, cityService){
+myWeatherApp.controller('homeController',['$scope', '$location', 'cityService', function($scope, $location, cityService){
     
     $scope.city = cityService.city;
     
     $scope.$watch('city', function(){
         cityService.city = $scope.city;
     });
+    
+    $scope.submit = function(){
+        $location.path("/forecast");
+    }
     
 }]);
 
